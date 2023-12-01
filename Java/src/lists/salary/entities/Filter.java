b4983b8f9ec2c845bd2b9ea72c7782126d24d13f
João Augusto <thegameshabbo@gmail.com>
2023-12-01 16:24:08 -0300

    Adjuste erros

---
+++ Java/src/lists/salary/entities/Filter.java
@@ -0,0 +1,9 @@
+package lists.salary.entities;
+
+import java.util.List;
+public class Filter {
+    public static boolean hasId(List<Employee> list, int id) {
+        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        return emp != null;
+    }
+}
